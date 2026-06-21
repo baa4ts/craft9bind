8d25757c0f027a426ab1c54c44ac19d07cea0c54
baa4ts <baa4tsdev@gmail.com>
2026-06-21 10:37:29 -0300

    Fix 2

--- src/helpers/generarZonas.ts
+++ src/helpers/generarZonas.ts
@@ -4,6 +4,7 @@ import type { ZoneRecord } from "@/type/Zona.interface";
 export const generarZonaDb = (
 	soa: SoaHead,
 	registros: ZoneRecord[],
+	nombre?: string,
 ): string => {
 	const records = registros
 		.map(
